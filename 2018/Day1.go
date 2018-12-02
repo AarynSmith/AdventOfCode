5d1f109ba5302e9aa245a6ebb96515a220710d28
Aaryn Smith <4387096+AarynSmith@users.noreply.github.com>
2018-12-01 21:26:33 -0600

    Code Refactor

--- 2018/Day1.go
+++ 2018/Day1.go
@@ -9,54 +9,41 @@ import (
 
 func main() {
 	dat, err := ioutil.ReadFile("input/Day1.txt")
-
 	if err != nil {
 		panic(err)
 	}
 	lines := strings.Split(string(dat), "\n")
 
-	FreqDict := map[int]int{}
-	DuplicateNotFound := true
 	CurrentFreq := 0
-	idx := 0
-	i := 0
-	// for i := 0; i < len(lines); i++{
-	for DuplicateNotFound {
+	FreqMap := map[int]int{};
+	FreqMap[CurrentFreq] += 1
+	idx := 0;
+	for {
 		if len(lines[idx]) == 0 {
-			i += 1
 			idx = (idx + 1) % len(lines)
 			continue
 		}
 
-		FreqDict[CurrentFreq] = FreqDict[CurrentFreq] + 1
-		if FreqDict[CurrentFreq] == 2 {
-			break
-		}
-
-		ChangeDir := lines[idx][0]
-		ChangeFreq, err := strconv.Atoi(lines[idx][1:])
+		FreqChange, err := strconv.Atoi(lines[idx])
 		if err != nil {
 			panic(err)
 		}
-		
-		NewFreq := 0
-		switch ChangeDir {
-		case '+':
-			NewFreq = CurrentFreq + ChangeFreq
-		case '-':
-			NewFreq = CurrentFreq - ChangeFreq
-		default:
-			panic("Unknown Direction: " + string(ChangeDir))
-		}
 
-		fmt.Printf("Loop: %d, Freq %d; Current frequency %d, change of %c%d; resulting freqency %d.\n",
-			(i / len(lines)), i%len(lines), CurrentFreq, ChangeDir, ChangeFreq, NewFreq,
+		NewFreq := CurrentFreq + FreqChange
+
+		fmt.Printf("Current frequency %d, change of %d; resulting freqency %d.\n",
+			CurrentFreq, FreqChange, NewFreq,
 		)
 
 		CurrentFreq = NewFreq
-		i += 1
+		FreqMap[CurrentFreq] += 1
+		if (FreqMap[CurrentFreq] > 1){
+			break
+		}
+
+
 		idx = (idx + 1) % len(lines)
 	}
 
-	fmt.Printf("Duplicated Frequency Found: %d.", CurrentFreq)
+	fmt.Printf("Final Frequency Found: %d.", CurrentFreq)
 }
